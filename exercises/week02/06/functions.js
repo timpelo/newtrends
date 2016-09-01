5cce2048eba82ad48aa887f7fbdec00cbd478596
Jani Timonen <janij.timonen@gmail.com>
2016-09-01 12:35:50 +0300

    6 ready

--- exercises/week02/06/functions.js
+++ exercises/week02/06/functions.js
@@ -3,12 +3,13 @@ function a() {
     console.log(jeppe.fname + " " + jeppe.lname);
 }
 
+
 function b() {
     var jeppe = {fname:"Jeppe", lname:"Mononen"};
     for(var name in jeppe) {
         console.log(name + " " + jeppe[name]);
     }
-    
+
 }
 
 function c() {
@@ -16,7 +17,7 @@ function c() {
     var mikko = {fname:"Mikko", lname:"Mallikas"};
     var jaska = {fname:"Jaska", lname:"Jokunen"};
     var personList = [jeppe, mikko, jaska];
-    
+
     for(var person in  personList) {
         for(var name in personList[person]) {
             console.log(name + " " + personList[person][name]);
@@ -24,9 +25,20 @@ function c() {
     }
 }
 
-function d(){
-    
-    function Person(fname, lname) {
-        
+function d() {
+
+    var Person = function (fname_, lname_) {
+        this.fname = fname_;
+        this.lname = lname_;
+        this.age = 0;
+
+        this.setAge = function(age_) {
+            this.age = age_;
+        }
     }
+
+    var person = new Person("Martti", "Lutteri");
+    person.setAge(15);
+    document.write(person.fname + " " + person.lname + " " + person.age);
+    console.log(window);
 }
